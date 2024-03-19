54deb4453b354ba3fd985714eb415a60024a2085
Ralph <rmilford@ualberta.ca>
2024-03-19 09:27:23 -0600

    Habits: App: Redid for testing purposes

--- DearHabits/src/App.jsx
+++ DearHabits/src/App.jsx
@@ -10,7 +10,7 @@ import MainView from './habits/ViewHabit'
 import Habit from './habits/classes/Habit'
 import HabitGrouping from './habits/classes/HabitGrouping'
 import ListHabits from './habits/ListHabits'
-import Home from './Home'
+import HabitPage from './habits/HabitPage'
 import Hotbar from './Hotbar'
 
 const group = [new HabitGrouping("Running", "Numerical"), 
@@ -34,33 +34,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <section>
-        {/* <MainCreate />
-        <MainView habit={habit} /> */}
-        {/* <ListHabits habits={habits} /> */}
-        {/* <Home habits={habits} /> */}
-        <Hotbar habits={habits} />
-      </section>
+      <Hotbar habits={habits} />
     </>
   )
 }
