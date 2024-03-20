97070f1110a6f9393a5c20da95ecbca709399995
Ralph <rmilford@ualberta.ca>
2024-03-20 17:23:04 -0600

    Habits: Merged Habits with the main branch and dealt with the merge conflicts

--- DearHabits/src/App.jsx
+++ DearHabits/src/App.jsx
@@ -1,31 +1,51 @@
-import './App.css'
-
-import Habit from './habits/classes/Habit'
-import HabitGrouping from './habits/classes/HabitGrouping'
-import ListHabits from './habits/ListHabits'
-import HabitPage from './habits/HabitPage'
-import Hotbar from './Hotbar'
-
-const group = [new HabitGrouping("Running", "Numerical"), 
-               new HabitGrouping("Mood", "Scale", "Good", "Bad", 3), 
-               new HabitGrouping("Weights", "Text"),
-               new HabitGrouping("Plank", "Checkmark")];
-const habit = new Habit("Exercise", "Weekly", "Private");
-const habits = [habit, new Habit("Sleep", "Daily", "Private"), new Habit("Eating", "Daily", "Public")];
-// group[0].values = [["2024-03-13", "2"], ["2024-03-14", "5"], ["2024-03-15", "3"], ["2024-03-16", "6"]];
-for (let i = 1; i < 66; i++) {
-  const date = new Date(2023, 1, i);
-  group[0].incrementValue([`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`, "2"]);
-}
-group[3].values = [["2024-03-13", "1"], ["2024-03-14", "1"], ["2024-03-15", "0"], ["2024-03-16", "1"]];
-habit.updateGroup(group);
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Registration from "./pages/registration";
+import Main from "./pages/main";
+import "./App.css";
+import { useEffect, useState } from "react";
+import ChangePassword from "./pages/changePassword";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+
   return (
-    <>
-      <Hotbar habits={habits} />
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                email={email}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+          />
+          <Route
+            path="/registration"
+            element={
+              <Registration setLoggedIn={setLoggedIn} setEmail={setEmail} />
+            }
+          />
+          <Route path="/main" element={<Main />} />
+          <Route
+            path="/changePassword"
+            element={
+              <ChangePassword />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
