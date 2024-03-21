ef5173cad9c05857331fbdef6f0183f564101312
joRusk <91704311+joRusk@users.noreply.github.com>
2024-03-21 10:03:41 -0600

    heroku commit 2

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5001
 
 express()
-//   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'public')))
 //   .set('views', path.join(__dirname, 'views'))
 //   .set('view engine', 'ejs')
 //   .get('/', (req, res) => res.render('pages/index'))
